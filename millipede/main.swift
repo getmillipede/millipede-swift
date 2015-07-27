76bcd235674ab86d047ea3eb16ecffe0b54747b3
Arnaud Chong <shine@achamo.net>
2015-07-27 14:13:42 +0200

    Initial Commit

---
+++ millipede/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  millipede
+//
+//  Created by Arnaud Chong on 27/07/2015.
+//  Copyright (c) 2015 Arnaud Chong. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
