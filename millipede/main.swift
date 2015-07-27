d6e32c760af85d475eb50197487b59584ff967ce
Arnaud Chong <shine@achamo.net>
2015-07-27 14:11:55 +0200

    millipede.swift

--- millipede/main.swift
+++ millipede/main.swift
@@ -8,5 +8,26 @@
 
 import Foundation
 
-println("Hello, World!")
+let offsets = [2, 1, 0, 1, 2, 3, 4, 4, 3]
+
+func print_body(size: Int) {
+    for _ in 0...size {
+        print(" ")
+    }
+    println("╚═(███)═╝")
+}
+
+func print_millipede(size: Int) {
+    println("    ╚⊙ ⊙╝")
+    for idx in 1...size {
+        print_body(offsets[idx % offsets.count])
+    }
+}
+
+if Process.arguments.count < 2 {
+    print_millipede(20)
+}
+else {
+    print_millipede(Process.arguments[1].toInt()!)
+}
 
